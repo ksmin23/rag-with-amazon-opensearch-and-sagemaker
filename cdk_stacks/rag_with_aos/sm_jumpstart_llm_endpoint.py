7d5932b3d89d61f93f8e8ebfa978fabd06829c93
ksmin23 <sonagy23@gmail.com>
2024-03-27 12:40:02 +0900

    take model id with cdk context variables

--- cdk_stacks/rag_with_aos/sm_jumpstart_llm_endpoint.py
+++ cdk_stacks/rag_with_aos/sm_jumpstart_llm_endpoint.py
@@ -26,13 +26,16 @@ class SageMakerJumpStartLLMEndpointStack(Stack):
   def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
 
+    jumpstart_model_id = self.node.try_get_context('jumpstart_llm_model_id') or 'META_TEXTGENERATION_LLAMA_2_7B_2_1_0'
+    llm_endpoint_name = self.node.try_get_context('llm_endpoint_name') or 'llama2-7b'
+
     RANDOM_GUID = ''.join(random.sample(string.digits, k=7))
-    endpoint_name = f"llama2-endpoint-{RANDOM_GUID}"
+    endpoint_name = f"{llm_endpoint_name}-{RANDOM_GUID}"
 
     #XXX: Available JumStart Model List
     # https://github.com/awslabs/generative-ai-cdk-constructs/blob/main/src/patterns/gen-ai/aws-model-deployment-sagemaker/jumpstart-model.ts
     llm_endpoint = JumpStartSageMakerEndpoint(self, 'LLMEndpoint',
-      model=JumpStartModel.META_TEXTGENERATION_LLAMA_2_7_B_2_1_0,
+      model=JumpStartModel.of(jumpstart_model_id.upper()),
       accept_eula=True,
       instance_type=SageMakerInstanceType.ML_G5_2_XLARGE,
       endpoint_name=endpoint_name
